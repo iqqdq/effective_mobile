556d6a1ad3471713a9954e3040329f7edf24644e
iqqdq <iqqddq@gmail.com>
2024-05-09 03:16:37 +0300

    Delete build directory

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/iqqddq/Documents/GitHub/effective_mobile/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/iqqddq/Documents/GitHub/effective_mobile/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/arm64/shared_preferences_foundation-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/iqqddq/Documents/GitHub/effective_mobile/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
-  /Users/iqqddq/Documents/GitHub/effective_mobile/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-dummy.m \
-  /Users/iqqddq/Documents/GitHub/effective_mobile/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-prefix.pch
